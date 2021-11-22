7fe06bee9a2e91deb0841bae539409a89db77c7a
pavel <makovpavel@rambler.ru>
2021-11-22 20:11:55 +0300

    1

--- settings.py
+++ settings.py
@@ -1,4 +1,9 @@
-host = 'bainel.ru'
-user = 'paxa'
-password = '523524'
-database = 'server'
+import pandas as pd
+
+
+a = pd.read_csv('settings.csv')
+host = str(a['host'][0])
+user = str(a['user'][0])
+password = str(a['password'][0])
+database = str(a['database'][0])
+print(host)
